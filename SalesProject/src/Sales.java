3d450dbc60235f86a7aa597aece3ad60527bfac0
Tedros <tedgetu.eyasu@gmail.com>
2018-06-03 21:03:04 -0400

    initial checkin of Sales

---
+++ SalesProject/src/Sales.java
@@ -0,0 +1,18 @@
+
+public class Sales {
+
+	
+	public void display() {
+		
+		System.out.println("Sales ..");
+	}
+
+	
+	public static void main(String[] args) {
+	
+		Sales s = new Sales();
+		
+		s.display();
+	}
+}
+
